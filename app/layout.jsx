25a00e467f2370c404a65633bd9c362629206422
Cale <cale.tian@icloud.com>
2023-03-15 20:09:45 -0700

    finished bio page and contact page(front end)

--- app/layout.jsx
+++ app/layout.jsx
@@ -42,15 +42,32 @@ export default function RootLayout({ children }) {
               <Link href="/contact" className='hover:text-orange-400 transition-colors'>Contact Me</Link>
               </li>
               <li>
-              <Link href="/" className='hover:text-orange-400 transition-colors'>About</Link>
+              <Link href="/about" className='hover:text-orange-400 transition-colors'>About</Link>
               </li>
-              {/* <li>
+              {/* <li>/dynasty-apr-13
               <Link href="/" className='text-orange-400 hover:text-orange-600'>Login</Link>
               </li> */}
           </ul>
         </nav>
         </div>
         {children}
+        <div className='flex flex-col items-center text-center md:flex-row md:items-start md:text-left md:justify-around lg:gap-20 gap-12 p-12 select-none'>
+          <div>
+            <h2 className='font-semibold text-lg mb-2'>Contact</h2>
+            <a href='mailto:cale@caletian.com' className='hover:text-orange-400 transition-colors'>cale@caletian.com</a>
+            <p>778-847-1060</p>
+          </div>
+          <div>
+            <h2 className='font-semibold text-lg mb-2'>Office Hour</h2>
+            <p>Monday - Saturday</p>
+            <p>10:00 AM - 6:00 PM</p>
+            <p className='text-sm font-thin'>Photoshoot times vary depends on projects.</p>
+          </div>
+          <div>
+            <h2 className='font-semibold text-lg mb-2'>Location</h2>
+            <p>Vancouver, BC</p>
+          </div>
+        </div>
       </body>
     </html>
   )
